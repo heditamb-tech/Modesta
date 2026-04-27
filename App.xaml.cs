a749a6b3d626184f3173b34a8949503533ab51c8
Heidi <Heidi@DESKTOP-J7O0UKF>
2026-04-27 12:38:16 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Modesta
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
